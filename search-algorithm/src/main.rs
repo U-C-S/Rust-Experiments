03aa71b0eaba479a623a0ea2dfedc36ef4fb898c
U-C-S <ucs3721@outlook.com>
2021-06-06 15:54:19 +0530

    successful binary-search

--- search-algorithm/src/main.rs
+++ search-algorithm/src/main.rs
@@ -9,9 +9,9 @@ fn main() {
     let size: i32 = read!();
 
     println!("Elements are: \n");
-    let mut arr: Vec<i16> = Vec::new();
+    let mut arr: Vec<u16> = Vec::new();
     for _i in 0..size {
-        let num = rand::thread_rng().gen_range(0..200);
+        let num = rand::thread_rng().gen_range(0..1000);
         print!("{} ", num);
         arr.push(num);
     }
@@ -22,12 +22,25 @@ fn main() {
         let opt: Option<i8> = Some(read!());
 
         match opt {
-            Some(1) => {}
+            Some(1) => {
+                let arr_sorted = sort(arr.clone());
+                print!("\nSorted list: ");
+                for i in &arr_sorted {
+                    print!("{} ", i);
+                }
+                println!("\nEnter the element to be searched: ");
+                let item: u16 = read!();
+                let now = Instant::now();
+                let pos = binary_search(&item, arr_sorted.clone());
+                let elapsed = now.elapsed();
+                println!("Elapsed: {:.2?}", elapsed);
+                println!("Element at index: {}", pos);
+            }
             Some(2) => {
                 println!("Enter the element to be searched: ");
-                let item: i16 = read!();
+                let item: u16 = read!();
                 let now = Instant::now();
-                let pos = linear_search(item, arr.clone());
+                let pos = linear_search(&item, arr.clone());
                 let elapsed = now.elapsed();
                 println!("Elapsed: {:.2?}", elapsed);
                 println!("Element at index: {}", pos);
@@ -39,12 +52,12 @@ fn main() {
     }
 }
 
-fn linear_search(item: i16, arr: Vec<i16>) -> isize {
-    let size = arr.len() as isize;
+fn linear_search(item: &u16, arr: Vec<u16>) -> usize {
+    let size = arr.len();
 
     for i in 0..size {
         sleep(Duration::from_millis(100));
-        let curr = *arr.get(i as usize).unwrap();
+        let curr = arr.get(i).unwrap();
         if item == curr {
             return i + 1;
         }
@@ -53,18 +66,39 @@ fn linear_search(item: i16, arr: Vec<i16>) -> isize {
     return 0;
 }
 
-pub fn binary_search<T: Ord>(item: &T, arr: &[T]) -> Option<usize> {
+pub fn binary_search(item: &u16, arr: Vec<u16>) -> usize {
     let mut left = 0;
     let mut right = arr.len();
 
     while left < right {
+        sleep(Duration::from_millis(100));
         let mid = left + (right - left) / 2;
 
         match item.cmp(&arr[mid]) {
             Ordering::Less => right = mid,
-            Ordering::Equal => return Some(mid),
+            Ordering::Equal => return mid + 1,
             Ordering::Greater => left = mid + 1,
         }
     }
-    None
+    return 0;
+}
+
+fn sort(arr: Vec<u16>) -> Vec<u16> {
+    let mut arr = arr;
+    let size = arr.len();
+    let mut i: usize = 0;
+    while i < size - 1 {
+        let mut j = i + 1;
+        while j < size {
+            if arr[i] > arr[j] {
+                arr.swap(i, j);
+            }
+
+            j += 1;
+        }
+
+        i += 1;
+    }
+
+    return arr;
 }
