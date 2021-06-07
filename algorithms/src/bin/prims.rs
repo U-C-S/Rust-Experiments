6730cfd416a15185baeb7cc0460b42e978842e79
U-C-S <ucs3721@outlook.com>
2021-06-07 18:16:33 +0530

    Wrote some part of the prims algo - I doubt its called prims algo - also for the first time, used todo macro

--- algorithms/src/bin/prims.rs
+++ algorithms/src/bin/prims.rs
@@ -1,3 +1,56 @@
+use text_io::read;
+
 fn main() {
-    println!("hello");
+    let mut minimum: usize = 999;
+
+    println!("Enter the number of vertices");
+    let n: usize = read!();
+
+    println!("Enter the Cost Matrix: ");
+    let mut cost_vector: Vec<Vec<usize>> = Vec::new();
+    let mut i: usize = 1;
+    let mut j: usize = 1;
+
+    while i <= n {
+        while j <= n {
+            cost_vector[i][j] = read!();
+            j += 1;
+        }
+        i += 1;
+    }
+
+    let mut count = 1;
+    println!("The Spanning tree's edges are: ");
+    while count < n {
+        minimum = 999;
+        i = 1;
+        j = 1;
+
+        while i <= n {
+            while j <= n {
+                if cost_vector[i][j] == minimum {
+                    if true {
+                        //todo
+                        todo!();
+                    } else {
+                        minimum = cost_vector[i][j];
+                        todo!();
+                    }
+                }
+                j += 1;
+            }
+            i += 1;
+        }
+
+        if true {
+            //todo
+            count += 1;
+            println!("Edge()"); //todo
+            todo!();
+        }
+
+        todo!();
+    }
+
+    println!("The minimum cost = {}", todo!());
 }
