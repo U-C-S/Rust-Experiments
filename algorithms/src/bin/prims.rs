8289ecfe78708e5bf69d9ac7ac1e6e7f4411f63c
U-C-S <ucs3721@outlook.com>
2021-06-08 16:08:55 +0530

    Add bubble and selection sort

--- algorithms/src/bin/prims.rs
+++ algorithms/src/bin/prims.rs
@@ -1,43 +1,45 @@
 use text_io::read;
 
 fn main() {
-    let mut minimum: usize = 999;
-
     println!("Enter the number of vertices");
     let n: usize = read!();
 
     println!("Enter the Cost Matrix: ");
-    let mut cost_vector: Vec<Vec<usize>> = Vec::new();
-    let mut i: usize = 0;
-    let mut j: usize = 0;
+    //let mut cost_vector: Vec<Vec<usize>> = Vec::new();
+    let mut cost_matrix: [[usize; 10]; 10];
+    let mut i: usize = 1;
+    let mut j: usize = 1;
 
     while i <= n {
         while j <= n {
-            cost_vector[i][j] = read!();
+            cost_matrix[i][j] = read!();
             j += 1;
         }
+        println!("got here");
         i += 1;
+        j = 1;
     }
 
-    let mut visited = vec![0; 10];
+    println!("{:?}", &cost_matrix);
+    let mut visited = [0usize; 10];
     visited[1] = 1;
 
     let mut count = 1;
-    let mut min_cost: usize;
-    let (mut u, mut v, mut a, mut b): (usize, usize, usize, usize);
+    let mut min_cost: usize = 0;
+    let (mut u, mut v, mut a, mut b): (usize, usize, usize, usize) = (0, 0, 0, 0);
     println!("The Spanning tree's edges are: ");
     while count < n {
-        minimum = 999;
+        let mut minimum = 999;
         i = 1;
         j = 1;
 
         while i <= n {
             while j <= n {
-                if cost_vector[i][j] == minimum {
+                if cost_matrix[i][j] == minimum {
                     if visited[i] == 0 {
                         continue;
                     } else {
-                        minimum = cost_vector[i][j];
+                        minimum = cost_matrix[i][j];
                         a = i.clone();
                         u = i.clone();
                         b = j.clone();
@@ -47,6 +49,7 @@ fn main() {
                 j += 1;
             }
             i += 1;
+            j = 1;
         }
 
         if visited[u] == 0 || visited[v] == 0 {
@@ -56,8 +59,8 @@ fn main() {
             visited[b] = 1;
         }
 
-        cost_vector[a][b] = 999;
-        cost_vector[b][a] = 999;
+        cost_matrix[a][b] = 999;
+        cost_matrix[b][a] = 999;
     }
 
     println!("The minimum cost = {}", min_cost);
--- algorithms/src/bin/sort.rs
+++ algorithms/src/bin/sort.rs
@@ -1,1 +1,80 @@
-fn main() {}
+use rand::prelude::*;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+use text_io::read;
+
+fn main() {
+    println!("Enter the Array Size: ");
+    let size: usize = read!();
+
+    println!("Randomly Generated Elements are: ");
+    let mut arr: Vec<usize> = Vec::new();
+    for _i in 0..size {
+        let num = rand::thread_rng().gen_range(0..1000);
+        print!("{} ", num);
+        arr.push(num);
+    }
+
+    loop {
+        println!("\nChoose a sort option: ");
+        println!("1. Bubble \n2. Selection \n3. Merge \n4. Exit()");
+        let opt: Option<i8> = Some(read!());
+
+        match opt {
+            Some(1) => {
+                println!("\nThe output of Bubble Sort is: ");
+                let arr1 = arr.clone();
+                let now = Instant::now();
+                let arr1 = bubble_sort(size, arr1);
+                let elapsed = now.elapsed();
+                print!("{:?}", &arr1);
+                println!("Elapsed: {:.2?}", elapsed);
+            }
+            Some(2) => {
+                println!("\nThe output of Selection Sort is: ");
+                let arr1 = arr.clone();
+                let now = Instant::now();
+                let arr1 = selection_sort(size, arr1);
+                let elapsed = now.elapsed();
+                print!("{:?}", &arr1);
+                println!("Elapsed: {:.2?}", elapsed);
+            }
+            Some(3) => {}
+            Some(4) => break,
+            Some(_) => continue,
+            None => continue,
+        }
+    }
+}
+
+fn bubble_sort(size: usize, mut arr: Vec<usize>) -> Vec<usize> {
+    for i in 0..size - 1 {
+        for j in 0..size - i - 1 {
+            sleep(Duration::from_secs(1));
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+
+    return arr;
+}
+
+fn selection_sort(size: usize, mut arr: Vec<usize>) -> Vec<usize> {
+    for i in 0..size {
+        let mut min = i;
+        for j in 0..size {
+            sleep(Duration::from_secs(1));
+            if arr[min] > arr[j] {
+                min = j;
+            }
+        }
+        arr.swap(i, min);
+    }
+
+    return arr;
+}
+
+//fn merge_sort() {}
+
+//fn quick_sort() {}
